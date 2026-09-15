4767589668e8a88a2f77bf768cd208edb2463ccd
Trang Dao <trangdao.01012006@gmail.com>
2026-09-15 13:08:10 -0700

    Lad_a

--- src/main/java/studentmanager/demo/DemoApplication.java
+++ src/main/java/studentmanager/demo/DemoApplication.java
@@ -21,50 +21,50 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	// http://localhost:8080/api/hello
-	@GetMapping("/hello")
-    public String hello() {
-        return "Hello Spring Boot API";
-    }
-	//Api voi Path Variable
-	//http://localhost:8080/api/student/10
-	@GetMapping("/student/{id}")
-	public String getStudent(@PathVariable int id) {
-		return "Sinh viên có mã: " + id;
-	}
-	//API nhan tham so vd1
-	//http://localhost:8080/api/student?name=Trang
-	@GetMapping("/student")
-	public String greet(@RequestParam String name) {
-    	return "Xin chào " + name;
-	}
-	//API nhan tham so vd2
-	//http://localhost:8080/api/searchstudent?name=Trang&age=20
-	@GetMapping("/searchstudent")
-	public String searchStudent(@RequestParam String name,
-                         @RequestParam(defaultValue = "1") int age) {
-    	return "Tên=" + name + ", tuổi=" + age;
-	}
-	//Tra ve Json object
-	//http://localhost:8080/api/students
-	@GetMapping("/students")
-	public Student getStudent() {
-    	return new Student(1,"Dao Thi Huyen Trang",20);
-	}
-	//API tra ve danh sach
-	//http://localhost:8080/api/studentall
-	@GetMapping("/studentall")
-	public List<Student> getStudents() {
-    	List<Student> list = new ArrayList<>();
-    	list.add(new Student(1, "Dao Thi Huen Trang", 20));
-    	list.add(new Student(2, "Nguyen Thi Mai Huyen", 21));
-    	return list;
-	}
-	//@RequestHeader  
-	//http://localhost:8080/api/getstudent
-	@GetMapping("/getstudent")
-	public String getStudents(
-        @RequestHeader("Authorization") String authorization) {
-    		return "Authorization = " + authorization;
-	}
+	// // http://localhost:8080/api/hello
+	// @GetMapping("/hello")
+    // public String hello() {
+    //     return "Hello Spring Boot API";
+    // }
+	// //Api voi Path Variable
+	// //http://localhost:8080/api/student/10
+	// @GetMapping("/student/{id}")
+	// public String getStudent(@PathVariable int id) {
+	// 	return "Sinh viên có mã: " + id;
+	// }
+	// //API nhan tham so vd1
+	// //http://localhost:8080/api/student?name=Trang
+	// @GetMapping("/student")
+	// public String greet(@RequestParam String name) {
+    // 	return "Xin chào " + name;
+	// }
+	// //API nhan tham so vd2
+	// //http://localhost:8080/api/searchstudent?name=Trang&age=20
+	// @GetMapping("/searchstudent")
+	// public String searchStudent(@RequestParam String name,
+    //                      @RequestParam(defaultValue = "1") int age) {
+    // 	return "Tên=" + name + ", tuổi=" + age;
+	// }
+	// //Tra ve Json object
+	// //http://localhost:8080/api/students
+	// @GetMapping("/students")
+	// public Student getStudent() {
+    // 	return new Student(1,"Dao Thi Huyen Trang",20);
+	// }
+	// //API tra ve danh sach
+	// //http://localhost:8080/api/studentall
+	// @GetMapping("/studentall")
+	// public List<Student> getStudents() {
+    // 	List<Student> list = new ArrayList<>();
+    // 	list.add(new Student(1, "Dao Thi Huen Trang", 20));
+    // 	list.add(new Student(2, "Nguyen Thi Mai Huyen", 21));
+    // 	return list;
+	// }
+	// //@RequestHeader  
+	// //http://localhost:8080/api/getstudent
+	// @GetMapping("/getstudent")
+	// public String getStudents(
+    //     @RequestHeader("Authorization") String authorization) {
+    // 		return "Authorization = " + authorization;
+	// }
 }
