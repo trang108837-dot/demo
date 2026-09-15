6b1cf567e66c899af7a1b023eea8c316822cb440
Trang Dao <trangdao.01012006@gmail.com>
2026-09-15 13:07:41 -0700

    Lad_A

--- src/main/java/studentmanager/demo/DemoApplication.java
+++ src/main/java/studentmanager/demo/DemoApplication.java
@@ -1,9 +1,15 @@
 package studentmanager.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +21,50 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	// http://localhost:8080/api/hello
 	@GetMapping("/hello")
     public String hello() {
         return "Hello Spring Boot API";
     }
-
+	//Api voi Path Variable
+	//http://localhost:8080/api/student/10
+	@GetMapping("/student/{id}")
+	public String getStudent(@PathVariable int id) {
+		return "Sinh viên có mã: " + id;
+	}
+	//API nhan tham so vd1
+	//http://localhost:8080/api/student?name=Trang
+	@GetMapping("/student")
+	public String greet(@RequestParam String name) {
+    	return "Xin chào " + name;
+	}
+	//API nhan tham so vd2
+	//http://localhost:8080/api/searchstudent?name=Trang&age=20
+	@GetMapping("/searchstudent")
+	public String searchStudent(@RequestParam String name,
+                         @RequestParam(defaultValue = "1") int age) {
+    	return "Tên=" + name + ", tuổi=" + age;
+	}
+	//Tra ve Json object
+	//http://localhost:8080/api/students
+	@GetMapping("/students")
+	public Student getStudent() {
+    	return new Student(1,"Dao Thi Huyen Trang",20);
+	}
+	//API tra ve danh sach
+	//http://localhost:8080/api/studentall
+	@GetMapping("/studentall")
+	public List<Student> getStudents() {
+    	List<Student> list = new ArrayList<>();
+    	list.add(new Student(1, "Dao Thi Huen Trang", 20));
+    	list.add(new Student(2, "Nguyen Thi Mai Huyen", 21));
+    	return list;
+	}
+	//@RequestHeader  
+	//http://localhost:8080/api/getstudent
+	@GetMapping("/getstudent")
+	public String getStudents(
+        @RequestHeader("Authorization") String authorization) {
+    		return "Authorization = " + authorization;
+	}
 }
---
+++ src/main/java/studentmanager/demo/Student.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package studentmanager.demo;
+
+
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+
+
+    public Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
